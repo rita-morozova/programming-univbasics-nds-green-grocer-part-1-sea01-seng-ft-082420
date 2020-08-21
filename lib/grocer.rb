bd9047a16cf59f41329028fca2089f8977b35824
Margarita Morozova <ritamorozova@yahoo.com>
2020-08-21 16:40:00 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,16 +1,15 @@
 def find_item_by_name_in_collection(name, collection)
-    collection.each do |item_info|
-  if item_info[:item] == name
-     return item_info
-end 
+  collection.each do |item_info|
+    if item_info[:item] == name  
+      return item_info 
+    end 
   end 
   nil
 end 
 
 def consolidate_cart(cart)
  new_cart = []
- 
-  cart.each do |cart_item|
+  cart.each do |cart_item| #iterate over each item in the cart
   item_name = cart_item[:item]
   item_info = find_item_by_name_in_collection(item_name, new_cart)
  if item_info
@@ -26,5 +25,3 @@ def consolidate_cart(cart)
   end 
   new_cart
 end 
-   
-   
